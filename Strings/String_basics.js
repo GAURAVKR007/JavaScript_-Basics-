c5ccde5164456825234afafd787ecc3f9ae22d4b
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-17 23:28:38 +0530

    String Basics Added

---
+++ Strings/String_basics.js
@@ -0,0 +1,8 @@
+// count no. of Characters
+var count = prompt("What is your name")
+alert("Characters typed : "+count.length);
+
+// var count = prompt("What is your name")
+// var total = 200;
+// alert("Characters typed : "+count.length+" Out of : "+total);
+// alert("You still had "+(total-count.length)+" Characters left");
