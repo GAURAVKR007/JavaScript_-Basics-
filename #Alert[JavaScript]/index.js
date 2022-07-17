1485bf412bb8fdc7651202c1660578eaf4768f58
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-17 23:16:25 +0530

    Some Basic code added

--- #Alert[JavaScript]/index.js
+++ #Alert[JavaScript]/index.js
@@ -4,3 +4,10 @@
 
 alert("Hello");
 alert("JavaScript");
+
+var myName = "Gaurav Kumar Thakur"
+alert(myName);
+alert("Says");
+alert("Hello");
+var yourName = prompt("What is your Name : ")
+alert(myName+" Says, Hello "+yourName)
