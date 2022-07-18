9e17718aa574f0be2f5af20d68f415a1abfa0da2
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-18 10:41:34 +0530

    Captailize the word

--- Strings/String_basics.js
+++ Strings/String_basics.js
@@ -6,3 +6,11 @@ alert("Characters typed : "+count.length);
 // var total = 200;
 // alert("Characters typed : "+count.length+" Out of : "+total);
 // alert("You still had "+(total-count.length)+" Characters left");
+
+// How to Capitalize Strings
+
+var name = prompt("What is Your Name : ");
+var first = name.slice(0,1);
+first = first.toUpperCase();
+var rest = name.slice(1,name.length).toLowerCase();
+alert("Hello "+first+rest);
